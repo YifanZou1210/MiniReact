1a2df1e46497195bda90660fc8bb1ef08a311819
Yifan Zou <zou.yifa@northeastern.edu>
2025-03-04 16:53:28 -0500

    add Backlog file to track development process, daily target and note

--- get-random-avatar/src/hooks/useFetchAvatar.jsx
+++ get-random-avatar/src/hooks/useFetchAvatar.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { fetchRandomAvatar } from '../services/unplashApi'
-
+/**
+ * Custom hooks in React encapsulate reusable logic
+ */
 //Encapsulate the logic of fetching an avatar image from an external service (in this case, the Unsplash API). 
 //This allows the component that uses this hook to focus only on rendering and UI logic
 //while the data-fetching logic (involving state management, async operations, etc.) is neatly abstracted and reusable.
