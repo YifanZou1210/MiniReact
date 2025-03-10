127ee0b4fb85987f337d217c4a1d0302eaa7ebb3
Yifan Zou <zou.yifa@northeastern.edu>
2025-03-10 00:12:29 -0400

    add TailwindCSS

--- get-random-avatar/src/App.jsx
+++ get-random-avatar/src/App.jsx
@@ -1,15 +1,22 @@
-// components/App.js
 import React from "react"
 import AvatarShow from './components/AvatarShow'
 import useFetchAvatar from "./hooks/useFetchAvatar"
 
-function App () {
+function App() {
   const { avatar, getAvatar } = useFetchAvatar()
   console.log('app includes', avatar)
+
   return (
-    <div>
-      <button onClick={getAvatar}>Pick Random Avatar</button>
-      <div>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-6">
+      <button
+        onClick={getAvatar}
+        className="px-6 py-3 mb-6 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-300 
+                   focus:outline-none focus:ring-4 focus:ring-blue-300"
+      >
+        Pick Random Avatar
+      </button>
+
+      <div className="flex justify-center items-center">
         <AvatarShow src={avatar} />
       </div>
     </div>
@@ -17,3 +24,4 @@ function App () {
 }
 
 export default App
+
