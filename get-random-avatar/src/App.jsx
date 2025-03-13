6e6dd6ff000cd364f7086db1e6eb3a5d531afade
Yifan Zou <zou.yifa@northeastern.edu>
2025-03-12 22:47:44 -0400

    Save Changes before Tailwind Upgrade

--- get-random-avatar/src/App.jsx
+++ get-random-avatar/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import AvatarShow from './components/AvatarShow'
 import useFetchAvatar from "./hooks/useFetchAvatar"
 
-function App() {
+function App () {
   const { avatar, getAvatar } = useFetchAvatar()
   console.log('app includes', avatar)
 
@@ -10,8 +10,8 @@ function App() {
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-6">
       <button
         onClick={getAvatar}
-        className="px-6 py-3 mb-6 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-300 
-                   focus:outline-none focus:ring-4 focus:ring-blue-300"
+        className="px-6 py-3 mb-6 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition duration-400 
+                   focus:outline-none"
       >
         Pick Random Avatar
       </button>
