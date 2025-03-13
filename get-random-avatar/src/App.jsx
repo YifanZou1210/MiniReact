5946f6e459b2db5a6c0c55cbf4954dc925f56090
Yifan Zou <zou.yifa@northeastern.edu>
2025-03-12 23:31:12 -0400

    Optimize Structure | Finish Basic Logic of Image Selector

--- get-random-avatar/src/App.jsx
+++ get-random-avatar/src/App.jsx
@@ -7,17 +7,17 @@ function App () {
   console.log('app includes', avatar)
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-6">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-yellow-100 to-green-200 p-6 relative">
       <button
         onClick={getAvatar}
-        className="px-6 py-3 mb-6 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition duration-400 
-                   focus:outline-none"
+        className="m-5 px-6 py-3 bg-gradient-to-r from-blue-500 via-blue-300 to-pink-300 bg-[length:200%_400%] animate-gradient-flow transition duration-800 rounded-xl text-amber-50 hover:bg-gradient-to-r hover:from-pink-300 hover:to-blue-300 hover:via-purple-300 hover:bg-[length:200%_200%] focus:outline-none
+        absolute top-4/5 transform -translate-y-1/2"
       >
         Pick Random Avatar
       </button>
 
-      <div className="flex justify-center items-center">
-        <AvatarShow src={avatar} />
+      <div className="flex justify-center items-center mt-10" >
+        <AvatarShow src={avatar} className="w-32 h-32 rounded-full border-4 border-white shadow-lg" />
       </div>
     </div>
   )
--- get-random-avatar/src/components/AvatarShow.jsx
+++ get-random-avatar/src/components/AvatarShow.jsx
@@ -1,8 +1,15 @@
-import React from "react"
-
 const AvatarShow = ({ src }) => {
-  console.log(src + 'is existed')
-  return src ? <img src={src} alt="Avatar" width={200} /> : <p>No Avatar</p>
-}
+  // 提取公共样式类
+  const avatarClasses = "w-32 h-32 rounded-full border-4 border-white absolute top-1/2 transform -translate-y-1/2 shadow-white-50 shadow-2xl"
 
-export default AvatarShow
+  // 使用传入的 src 或者默认图片
+  const avatarSrc = src || "/beach sunset.jpg"  // 请确保默认图片路径正确
+  return (
+    <img
+      src={avatarSrc}
+      alt="Avatar"
+      className={avatarClasses}
+    />
+  )
+};
+
