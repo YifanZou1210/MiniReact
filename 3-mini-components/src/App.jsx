fbc1935ce0186f4c2bf92db2f2d0adcea4a0d9b6
Yifan Zou <zou.yifa@northeastern.edu>
2025-04-09 00:25:00 -0400

    initialize configuration of mini-components branch - basic accordion

---
+++ 3-mini-components/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+/**
+ * AccordionItem 组件
+ * @param {object} props - 组件的属性对象
+ * @param {string} props.title - accordion item 的标题
+ * @param {React.ReactNode} props.children - accordion item 的内容
+ * @param {boolean} props.isOpen - accordion item 是否展开
+ * @param {function} props.onToggle - 切换 accordion item 展开状态的函数
+ */
+export const AccordionItem = ({title, children}) => {
+  
+};
